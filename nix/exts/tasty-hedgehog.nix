3c17d0e6879d5f25f08008d66595191f99b6cb28
Jake Leach <jacobleach@protonmail.com>
2023-01-02 14:45:07 -0600

    fix: GHC `9.4.2` compatibility

--- nix/exts/tasty-hedgehog.nix
+++ nix/exts/tasty-hedgehog.nix
@@ -6,7 +6,7 @@ final: prev:
   haskell = prev.haskell // {
     packages = prev.haskell.packages // {
       "${ghc}" = prev.haskell.packages."${ghc}".extend (self: _: {
-        prim-swar = self.callPackage ../pkgs/prim-swar.nix { };
+        tasty-hedgehog = self.callPackage ../pkgs/tasty-hedgehog.nix { };
       });
     };
   };
--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -3,8 +3,7 @@ args:
 import (import ./nixpkgs.nix) {
   config.packageOverrides = pkgs: 
     pkgs.lib.composeManyExtensions (map (f: f args) [  
-      (import exts/prim-swar.nix)
+      (import exts/tasty-hedgehog.nix)
       (import exts/utf8-text.nix)
-      (import exts/swar.nix)
     ]) pkgs pkgs;
 }
